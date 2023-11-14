9849a02c09976adfcc7f635fc886e3eaf97b7580
sekiin-sk <150459828+sekiin-sk@users.noreply.github.com>
2023-11-14 15:37:55 -0300

    Update 6 princ.py
    #add menu youtube

--- princ.py
+++ princ.py
@@ -4,11 +4,12 @@ import importlib
 
 
 def menu_principal():
-    print("Qual a plataforma?")
+    print("Qual a plataforma deseja usar?")
     print("     ")
     print("1 - Scribd")
     print("2 - Passei Direto")
     print("3 - Instagram - Video")
+    print("4 - YouTube")
     print("0 - Sair")
     print("     ")
 
@@ -23,12 +24,21 @@ def submenu_scribd():
 
 def submenu_pd():
     print("     ")
-    print("___Passei Direto_____")
+    print("------Passei Direto------")
     print("     ")
     print("1 - Desabilitado")
     print("2 - Desabilitado")
     print("3 - Voltar ao menu principal")
     print("     ")
+    
+def submenu_youtube():
+        print("     ")
+        print("------Youtube------")
+        print("     ")
+        print("1 - Video e Audio")
+        print("2 - PlayList")
+        print("3 - Voltar ao menu principal")
+        print("     ")
     
 def submenu_insta():
         print("     ")
@@ -118,6 +128,42 @@ def main():
 
                 else:
                     print("Opção inválida. Tente novamente.")
+                    
+        elif opcao_principal == "4":
+            while True:
+                submenu_youtube()
+                opcao_link = input("Escolha uma opção: ")
+
+                if opcao_link == "1":
+                    print("     ")
+                    # Coloque aqui o código para editar o link
+                    # Importação do arquivo 
+                    nome_do_modulo = "yt"
+                    modulo_importado = importlib.import_module(nome_do_modulo)
+                    
+
+                    database_connection.close()
+
+                    # Chamar a função main() do módul
+                    modulo_importado.main()
+
+                elif opcao_link == "2":
+                    print("    ")
+                    # Coloque aqui o código para copiar o link
+                    nome_do_modulo = "play"
+                    modulo_importado = importlib.import_module(nome_do_modulo)
+                    
+
+                    database_connection.close()
+
+                    # Chamar a função main() do módul
+                    modulo_importado.main()
+
+                elif opcao_link == "3":
+                    break  # Volta ao menu principal
+
+                else:
+                    print("Opção inválida. Tente novamente.")            
 
         elif opcao_principal == "0":
             print("Saindo...")
